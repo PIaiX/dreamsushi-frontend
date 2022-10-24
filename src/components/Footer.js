3cb8081e00b04a85698398af8339c612c4c6fa98
ngf93 <nabiullinaguzel93@gmail.com>
2022-10-24 19:58:54 +0300

    fix: footer img url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,7 +51,7 @@ export default function Footer() {
                     </Col>
                     <Col>
                         <a target='_blank' href='https://vk.com/market-157878257' className='d-flex align-items-center justify-content-end'>
-                            <img src='../imgs/icons/vk.svg' alt='Мы во Вконтакте'/>
+                            <img src='imgs/icons/vk.svg' alt='Мы во Вконтакте'/>
                             <div className='ms-2'>
                                 <div className='fw-5'>Мы во Вконтакте</div>
                                 <div className='light-gray fs-08'>+&nbsp;<span className='fw-6 font-color'>9 000</span> участников</div>
