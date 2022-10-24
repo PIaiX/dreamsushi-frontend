a63cd6cb9791504f92ffc8b118e1440f1ede3d64
ngf93 <nabiullinaguzel93@gmail.com>
2022-10-24 19:44:29 +0300

    Feature: order confirmation in process

---
+++ src/pages/Сonfirmation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Сonfirmation = () => {
+    return (
+        <main>
+            <Container>
+                <section className='mb-6'>
+                    <Row>
+                        <Col md={4}></Col>
+                        <Col md={7}></Col>
+                    </Row>
+                </section>
+            </Container>
+        </main>
+    );
+};
+
+export default Сonfirmation;
