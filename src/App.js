497f1fccd9bd26ddae7784ad08a1a319b7629e0b
ngf93 <nabiullinaguzel93@gmail.com>
2022-10-24 19:55:42 +0300

    fix: router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import AppRouter from './routes/AppRouter';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
