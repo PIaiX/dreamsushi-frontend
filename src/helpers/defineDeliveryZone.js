dd3fc80594fda5906589d3e82afd91be77d87eb6
Gikami <gikami.book@gmail.com>
2022-11-17 11:26:41 +0300

    production 1

--- src/helpers/defineDeliveryZone.js
+++ src/helpers/defineDeliveryZone.js
@@ -2,16 +2,18 @@ import * as Geolib from 'geolib'
 import Geo from '../config/geo.json'
 
 const defineDeliveryZone = (props) => {
-    if (props) {
+
+    if (!props) {
         return 'Нет props'
     }
     const data = Geo.features
     if (!data || data.length === 0) {
         return 'Нет данных features'
     }
+
     var zone = {
         name: 'Неизвестная зона',
-        orgId: null,
+        affiliate: '',
         description: 'Доставка не предусмотрена',
         status: false,
         price: 0,
@@ -22,7 +24,7 @@ const defineDeliveryZone = (props) => {
 
     data.map((item) => {
         Array(item).map((poly) => {
-            coordinates[polyId] = poly.geometry.coordinates[0]
+            coordinates[polyId] = poly.geometry.coordinates
             dataZone[polyId] = poly.properties
             polyId++
         })
@@ -31,8 +33,9 @@ const defineDeliveryZone = (props) => {
     if (!coordinates || coordinates.length === 0) {
         return 'Нет координат зон'
     }
+
     coordinates.map((item, i) => {
-        let dist = Geolib.isPointInPolygon({latitude: props.latitude, longitude: props.longitude}, item)
+        let dist = Geolib.isPointInPolygon({ latitude: props.lat, longitude: props.lon }, item)
         if (dist) {
             zone = dataZone[i] ? dataZone[i] : false
         }
